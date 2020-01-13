a1c89d858d61e1b4214cee6fe8895dcd90f3d84a
euijunh <lalaph@naver.com>
2020-01-13 10:43:00 +0900

    home 페이지에서 중복 id 문제 해결 - new 클래스

--- Page/New.js
+++ Page/New.js
@@ -10,15 +10,15 @@ class New extends ClickMore {
         this.$parent = param.parent;
         this.$el;
         this._render();
-        this.$more = this.$el.querySelector('#more');
-        this.$click = this.$el.querySelector('#click');
-        this.$scroll = this.$el.querySelector('#scroll');
+        this.$more = this.$el.querySelector('.more');
+        this.$click = this.$el.querySelector('.click');
+        this.$scroll = this.$el.querySelector('.scroll');
         this.url = 'https://my-json-server.typicode.com/it-crafts/mockapi/feed/';
         this.event = {
             initScroll: undefined,
         }
         this._create();
-        this.items = new Feed({ parent: this.$el.querySelector('#items') });
+        this.items = new Feed({ parent: this.$el.querySelector('.items') });
     }
 
     async _create() {
@@ -45,11 +45,11 @@ class New extends ClickMore {
             <div class="v9tJq VfzDr">
                 <div class=" _2z6nI">
                     <div class="Gx7Kn">
-                        <div id="items" style="flex-direction: column; padding-bottom: 0px; padding-top: 0px;">
+                        <div id="" class="items" style="flex-direction: column; padding-bottom: 0px; padding-top: 0px;">
                         </div>
-                        <div id="more" class=" Igw0E rBNOH YBx95 ybXk5 _4EzTm soMvl " style="margin-right: 8px">
-                            <button id="click" class="sqdOP L3NKy y3zKF _4pI4F" type="button" style="margin: 16px 8px">더보기</button>
-                            <button id="scroll" class="sqdOP L3NKy y3zKF _4pI4F" type="button" style="margin: 16px 8px">전체보기</button>
+                        <div id="" class=" Igw0E rBNOH YBx95 ybXk5 _4EzTm soMvl more" style="margin-right: 8px">
+                            <button id="" class="sqdOP L3NKy y3zKF _4pI4F click" type="button" style="margin: 16px 8px">더보기</button>
+                            <button id="" class="sqdOP L3NKy y3zKF _4pI4F scroll" type="button" style="margin: 16px 8px">전체보기</button>
                         </div>
                     </div>
                 </div>
