0ffc8c9a1ae48a103f47842f0079a068d94367d3
euijunh <lalaph@naver.com>
2020-01-13 10:44:48 +0900

    home 페이지에서 중복 id 문제 해결 - theme 클래스, theme 클래스 네이밍귳기 적용

--- Page/Theme.js
+++ Page/Theme.js
@@ -9,27 +9,27 @@ class Theme extends ClickMore {
         super();
         this.$parent = param.parent;
         this.$el;
-        this.render();
-        this.$more = this.$el.querySelector('#more');
-        this.$click = this.$el.querySelector('#click');
-        this.$scroll = this.$el.querySelector('#scroll');
+        this._render();
+        this.$more = this.$el.querySelector('.more');
+        this.$click = this.$el.querySelector('.click');
+        this.$scroll = this.$el.querySelector('.scroll');
         this.url = 'https://my-json-server.typicode.com/it-crafts/mockapi/feed/';
         this.event = {
             initScroll: undefined,
         }
-        this.create();
-        this.items = new Card({ parent: this.$el.querySelector('#items'), colsCnt: 3 });
+        this._create();
+        this.items = new Card({ parent: this.$el.querySelector('.items'), colsCnt: 3 });
     }
 
-    async create() {
+    async _create() {
         await super.create();
         if(this.hasNext) {
-            this.addEvent();
+            this._addEvent();
         } else {
             this.$more.style.display = 'none';
         }
     }
-
+    
     renderMore(data) {
         this.items.render(data);
     }
@@ -43,49 +43,16 @@ class Theme extends ClickMore {
         });
     }
 
-    initScroll(e) {
-        this.$more.style.display = 'none';
-        this.removeEvent();
-
-        if(Object.setPrototypeOf && Object.getPrototypeOf) {
-            Object.setPrototypeOf(Object.getPrototypeOf(this), ScrollMore.prototype);
-        } else {
-            this.__proto__.__proto__ = ScrollMore.prototype;
-        }
-
-        super.addEvent();
-        this.scroll();
-    }
-
-    finalScroll() {
-        if(Object.setPrototypeOf && Object.getPrototypeOf) {
-            Object.setPrototypeOf(Object.getPrototypeOf(this), ClickMore.prototype);
-        } else {
-            this.__proto__.__proto__ = ClickMore.prototype;
-        }
-    }
-
-    addEvent() {
-        super.addEvent();
-        this.event.initScroll = this.initScroll.bind(this);
-        this.$scroll.addEventListener('click', this.event.initScroll);
-    }
-
-    removeEvent() {
-        super.removeEvent();
-        this.$scroll.removeEventListener('click', this.event.initScroll);
-    }
-
-    render() {
+    _render() {
         const template = `
             <div class="v9tJq VfzDr">
                 <div class=" _2z6nI">
                     <div class="Gx7Kn">
-                        <div id="items" style="flex-direction: column; padding-bottom: 0px; padding-top: 0px;">
+                        <div id="" class="items" style="flex-direction: column; padding-bottom: 0px; padding-top: 0px;">
                         </div>
-                        <div id="more" class=" Igw0E rBNOH YBx95 ybXk5 _4EzTm soMvl " style="margin-right: 8px">
-                            <button id="click" class="sqdOP L3NKy y3zKF _4pI4F" type="button" style="margin: 16px 8px">더보기</button>
-                            <button id="scroll" class="sqdOP L3NKy y3zKF _4pI4F" type="button" style="margin: 16px 8px">전체보기</button>
+                        <div id="" class=" Igw0E rBNOH YBx95 ybXk5 _4EzTm soMvl more" style="margin-right: 8px">
+                            <button id="" class="sqdOP L3NKy y3zKF _4pI4F click" type="button" style="margin: 16px 8px">더보기</button>
+                            <button id="" class="sqdOP L3NKy y3zKF _4pI4F scroll" type="button" style="margin: 16px 8px">전체보기</button>
                         </div>
                     </div>
                 </div>
@@ -96,11 +63,46 @@ class Theme extends ClickMore {
         this.$el = elements[0];
     }
 
+    _initScroll() {
+        this.$more.style.display = 'none';
+        this._removeEvent();
+
+        // HACK 런타임 부모클래스 강제변경
+        if(Object.setPrototypeOf && Object.getPrototypeOf) {
+            Object.setPrototypeOf(Object.getPrototypeOf(this), ScrollMore.prototype);
+        } else {
+            this.__proto__.__proto__ = ScrollMore.prototype;
+        }
+
+        super.addEvent();
+        this.event.scroll(); // 스크롤 1회 강제호출
+    }
+
+    _finalScroll() {
+        // HACK 런타임 부모클래스 강제변경
+        if(Object.setPrototypeOf && Object.getPrototypeOf) {
+            Object.setPrototypeOf(Object.getPrototypeOf(this), ClickMore.prototype);
+        } else {
+            this.__proto__.__proto__ = ClickMore.prototype;
+        }
+    }
+
+    _addEvent() {
+        super.addEvent();
+        this.event.initScroll = this._initScroll.bind(this);
+        this.$scroll.addEventListener('click', this.event.initScroll);
+    }
+
+    _removeEvent() {
+        super.removeEvent();
+        this.$scroll.removeEventListener('click', this.event.initScroll);
+    }
+    
     destroy() {
         this.items.destroy();
         this.$parent.removeChild(this.$el);
-        this.removeEvent();
-        this.finalScroll();
+        this._removeEvent();
+        this._finalScroll();
     }
 }
 
