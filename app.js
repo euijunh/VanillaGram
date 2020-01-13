9fb0a9aefc8042bbe291bbbb52251e8fd12d6d58
euijunh <lalaph@naver.com>
2020-01-13 16:00:47 +0900

    app 라우트 pathname 세팅

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ class Root {
         current.firstChild.className = current.firstChild.className.replace('outline', 'filled');
         this._route(current.pathname);
 
-        history.pushState({tabIndex : index}, document.title, `project1${current.pathname}`);
+        history.pushState({tabIndex : index}, document.title, current.pathname);
     }
 
     _route(path) {
