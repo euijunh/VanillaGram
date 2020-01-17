36fad6c5bc5bf715d65bc1804ae59f9b588975ef
euijunh <lalaph@naver.com>
2020-01-17 20:02:18 +0900

    More 클래스 네이밍 규칙 적용

--- Prototype/More.js
+++ Prototype/More.js
@@ -7,41 +7,38 @@ class More {
         this.totalPage = 0;
         this.hasNext = undefined;
     }
-    
+
     async create() {
-        await Promise.all([this.fetchTotalPage(), this.ajaxMore()]);
+        await Promise.all([this._fetchTotalPage(), this.ajaxMore()]);
         const hasNext = this.page < this.totalPage;
         this.hasNext = hasNext;
     }
 
     async ajaxMore() {
-        const data = await this.fetchNextData();
+        const data = await this._fetchNextData();
         this.renderMore(data);
         const hasNext = this.page < this.totalPage;
         this.hasNext = hasNext;
         return hasNext;
     }
 
-    renderMore(data) {
-        throw new Error('오버라이드 되지 않은 추상메소드 호출됨');
-    }
-
-    async fetchNextData() {
+    async _fetchNextData() {
         const data = await common.getData(this.url, ++this.page);
         const list = Array.isArray(data) ? data : [data];
         this.processData(list);
         this.data.push(...list);
         return list;
     }
-
-    processData(list) {
-    }
-
-    async fetchTotalPage() {
+    
+    async _fetchTotalPage() {
         const info = await common.getInfo(this.url);
         const totalPage = info.totalPage * 1;
         this.totalPage = totalPage;
         return totalPage;
+    }
+
+    renderMore(data) {
+        throw new Error('오버라이드 되지 않은 추상메소드 호출됨');
     }
 }
 
